540d93c6d271f61baa3f799d0f33c98b14e905b0
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-17 10:54:36 -0700

    Contact page format

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   get '/music', to: 'music#index'
-  get '/shows', to: 'show#index'
+  get '/shows', to: 'shows#index'
   ## resources for shows instead?
   get '/contact', to: 'contact#index'
 end
