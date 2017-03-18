ba602c227607841888150c3bd411770e36a7bfce
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-18 13:43:55 -0700

    Add x to close div

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,9 +4,9 @@ class SubscriptionsController < ApplicationController
     begin
         ## in the model??
       gb.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address: email_params[:email], status: "subscribed"})
-      flash[:notice] = "Subscribed!"
+      flash[:notice] = "Subscribed!   X"
     rescue Gibbon::MailChimpError => e
-      flash[:notice] = JSON.parse(e.raw_body)['title']
+      flash[:notice] = JSON.parse(e.raw_body)['title'] + "    X"
     end
     redirect_to contact_path
   end
